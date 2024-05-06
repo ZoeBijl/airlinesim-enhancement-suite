a5d5cb7b3ac407a35c3e1943ed7ddc292ff92408
Zoë Bijl <github@hello.moiety.me>
2024-05-06 11:22:27 +1000

    feat: add currency format helper

--- extension/helpers.js
+++ extension/helpers.js
@@ -1,4 +1,40 @@
 /** Shared logic */
 class AES {
-    
+    /**
+     * Formats a currency value local standards
+     * @param {integer} currency value
+     * @param {string} alignment: "right" | "left"
+     * @returns {HTMLElement} span with formatted value
+     */
+    static formatCurrency = (value, alignment) => {
+        let container = document.createElement("span")
+        let formattedValue = Intl.NumberFormat().format(value)
+        let indicatorEl = document.createElement("span")
+        let valueEl = document.createElement("span")
+        let currencyEl = document.createElement("span")
+        let containerClasses = "aes-no-text-wrap"
+        
+        if (alignment === "right") {
+            containerClasses = "aes-text-right aes-no-text-wrap"
+        }
+        
+        if (value >= 0) {
+            valueEl.classList.add("good")
+            indicatorEl.innerText = "+"
+        }
+        
+        if (value < 0) {
+            valueEl.classList.add("bad")
+            indicatorEl.innerText = "-"
+            formattedValue = formattedValue.replace("-", "")
+        }
+        
+        valueEl.innerText = formattedValue
+        currencyEl.innerText = " AS$"
+        
+        container.className = containerClasses
+        container.append(indicatorEl, valueEl, currencyEl)
+        
+        return container
+    }
 }

3bce92293bc81f35cc7d155c5faa99c61e5a1493
Zoë Bijl <github@hello.moiety.me>
2024-05-22 09:31:36 +1000

    feat: add route management option buttons

--- extension/modules/dashboard_tabpanels.js
+++ extension/modules/dashboard_tabpanels.js
@@ -60,7 +60,7 @@ class DashboardTabpanels {
     async #createRouteManagement(container) {
         // TODO: Move to onload/oninstall?
         if(!settings.routeManagement){
-            setDefaultRouteManagementSettings();
+            setDefaultRouteManagementSettings()
         }
         
         let scheduleKey = server+airline.code+'schedule'
@@ -74,132 +74,134 @@ class DashboardTabpanels {
             
             container.append(p)
         } else {
-            container.append(generateRouteManagementTable(scheduleData[scheduleKey]))
+            let table = generateRouteManagementTable(scheduleData[scheduleKey])
+            let row = document.createElement("div")
+            row.className = "row"
+            let options = this.#createRouteManagementOptions()
+            let filters = displayRouteManagementFilters()
             
-            // Option buttons
-            /*let fieldsetEl = document.createElement("fieldset")
-            let legendEl = document.createElement("legend")
-            let buttonGroupEl = document.createElement("div")
+            row.append(options, filters)
             
-            let buttonElements = {
-                "selectFirstTen": {
-                    "label": "select first 10"
-                },
-                "hideChecked": {
-                    "label": "hide checked"
-                },
-                "openInventory": {
-                    "label": "open inventory (max 10)"
-                },
-                "reloadTable": {
-                    "label": "reload table"
-                }
-            }
-            
-            for (let key in buttonElements) {
-                let buttonObj = buttonElements[key]
-                let buttonEl = document.createElement("button")
-                let buttonClassNames = buttonObj?.classNames
-                let buttonType = buttonObj?.type
-                let buttonDefaultClassNames = "btn btn-default"
-                buttonEl.innerText = buttonObj.label
-            
-                if (buttonType) {
-                    buttonEl.setAttribute("type", buttonType)
-                } else {
-                    buttonEl.setAttribute("type", "button")
-                }
-            
-                if (buttonClassNames) {
-                    buttonEl.className = buttonClassNames
-                } else {
-                    buttonEl.className = buttonDefaultClassNames
-                }
-            
-                buttonObj.element = buttonEl
-            
-                buttonGroupEl.append(buttonEl)
-            }
-            
-            legendEl.innerText = "Options"
-            buttonGroupEl.classList.add("btn-group")
-            
-            fieldsetEl.append(legendEl, buttonGroupEl)
-            
-            let optionsDiv = $('<div class="col-md-4"></div>').append(fieldsetEl);
-            
-            // Button actions
-            
-            // Select first ten
-            buttonElements["selectFirstTen"].element.addEventListener("click", function() {
-                let count = 0
-                $('#aes-table-routeManagement tbody tr').each(function() {
-                    $(this).find("input").prop('checked', true);
-                    count++;
-                    if (count > 10) {
-                        return false;
-                    }
-                })
-            });
-            
-            // Remove checked
-            buttonElements["hideChecked"].element.addEventListener("click", function() {
-                $('#aes-table-routeManagement tbody tr').has('input:checked').remove();
-            });
-            
-            // Open Inventory
-            buttonElements["openInventory"].element.addEventListener("click", function() {
-                //Get checked collumns
-                let pages = $('#aes-table-routeManagement tbody tr').has('input:checked').map(function() {
-                    let orgdest = $(this).attr('id');
-                    orgdest = orgdest.split("-");
-                    orgdest = orgdest[2];
-                    //let orgdest = $(this).find("td:eq(1)").text() + $(this).find("td:eq(2)").text();
-                    let url = 'https://' + server + '.airlinesim.aero/app/com/inventory/' + orgdest;
-                    return url;
-                }).toArray();
+            container.append(row, table)
+        }
+    }
     
-                //Open new tabs
-                for (let i = 0; i < pages.length; i++) {
-                    window.open(pages[i], '_blank');
-                    if (i == 10) {
-                        break;
-                    }
-                }
-            })*/
+    #createRouteManagementOptions() {
+        let cell = document.createElement("div")
+        cell.className = "col-md-4"
+        let fieldsetEl = document.createElement("fieldset")
+        let legendEl = document.createElement("legend")
+        let buttonGroupEl = document.createElement("div")
+        
+        let buttonElements = {
+            "selectFirstTen": {
+                "label": "select first 10"
+            },
+            "hideChecked": {
+                "label": "hide checked"
+            },
+            "openInventory": {
+                "label": "open inventory (max 10)"
+            },
+            "reloadTable": {
+                "label": "reload table"
+            }
         }
         
+        for (let key in buttonElements) {
+            let buttonObj = buttonElements[key]
+            let buttonEl = document.createElement("button")
+            let buttonClassNames = buttonObj?.classNames
+            let buttonType = buttonObj?.type
+            let buttonDefaultClassNames = "btn btn-default"
+            buttonEl.innerText = buttonObj.label
         
-        /*
-              // Reload table reloadTable
-              buttonElements["reloadTable"].element.addEventListener("click", function(){
-                generateRouteManagementTable(scheduleData);
-              });
-              let divRow = $('<div class="row"></div>').append(optionsDiv,displayRouteManagementFilters(),displayRouteManagementCollumns())
-              div.prepend(divRow);
-              //Collumns selector Checkbox listener
-              $('#aes-table-routeManagement-collumns input').change(function(){
-                let show;
-                if(this.checked) {
-                  show = 1;
-                } else {
-                  show = 0;
-                }
-                let value = $(this).val();
-                settings.routeManagement.tableCollumns.forEach(function(col){
-                  if(col.class == value){
-                    col.show = show;
-                  }
-                });
-                chrome.storage.local.set({settings: settings}, function(){});
-              });
-        
+            if (buttonType) {
+                buttonEl.setAttribute("type", buttonType)
             } else {
-              //no schedule
-              div.append("Need schedule info to show this section. Change Dashboard to General -> Schedule -> Extract Schedule.")
+                buttonEl.setAttribute("type", "button")
             }
-          });
-          */
+        
+            if (buttonClassNames) {
+                buttonEl.className = buttonClassNames
+            } else {
+                buttonEl.className = buttonDefaultClassNames
+            }
+        
+            buttonObj.element = buttonEl
+        
+            buttonGroupEl.append(buttonEl)
+        }
+        
+        legendEl.innerText = "Options"
+        buttonGroupEl.classList.add("btn-group")
+        
+        fieldsetEl.append(legendEl, buttonGroupEl)
+        
+        // Select first ten
+        buttonElements["selectFirstTen"].element.addEventListener("click", function() {
+            let count = 0
+            $('#aes-table-routeManagement tbody tr').each(function() {
+                $(this).find("input").prop('checked', true)
+                count++
+                if (count > 10) {
+                    return false
+                }
+            })
+        })
+            
+        // Remove checked
+        buttonElements["hideChecked"].element.addEventListener("click", function() {
+            $('#aes-table-routeManagement tbody tr').has('input:checked').remove()
+        })
+            
+        // Open Inventory
+        buttonElements["openInventory"].element.addEventListener("click", function() {
+            //Get checked collumns
+            let pages = $('#aes-table-routeManagement tbody tr').has('input:checked').map(function() {
+                let orgdest = $(this).attr('id')
+                orgdest = orgdest.split("-")
+                orgdest = orgdest[2]
+                //let orgdest = $(this).find("td:eq(1)").text() + $(this).find("td:eq(2)").text();
+                let url = 'https://' + server + '.airlinesim.aero/app/com/inventory/' + orgdest
+                return url
+            }).toArray()
+    
+            //Open new tabs
+            for (let i = 0; i < pages.length; i++) {
+                window.open(pages[i], '_blank')
+                if (i == 10) {
+                    break
+                }
+            }
+        })
+        
+        // Reload table reloadTable
+        // buttonElements["reloadTable"].element.addEventListener("click", function() {
+        //     generateRouteManagementTable(scheduleData);
+        // });
+        // let divRow = $('<div class="row"></div>').append(optionsDiv, displayRouteManagementFilters(), displayRouteManagementCollumns())
+        // div.prepend(divRow)
+        // //Collumns selector Checkbox listener
+        // $('#aes-table-routeManagement-collumns input').change(function() {
+        //     let show;
+        //     if (this.checked) {
+        //         show = 1
+        //     } else {
+        //         show = 0
+        //     }
+        //     let value = $(this).val();
+        //     settings.routeManagement.tableCollumns.forEach(function(col) {
+        //         if (col.class == value) {
+        //             col.show = show
+        //         }
+        //     });
+        //     chrome.storage.local.set({ settings: settings }, function() {})
+        // })
+        
+        cell.append(fieldsetEl)
+        
+        return cell
     }
     
     #createCompetitorMonitor(container) {

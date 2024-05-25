c1d10a7ed9ae0676f8ee0dc9c6f74541aca95948
Zoë Bijl <github@hello.moiety.me>
2024-05-26 01:38:40 +1000

    refactor: pull the general dashboard into own class

--- extension/modules/dashboard_tabpanels.js
+++ extension/modules/dashboard_tabpanels.js
@@ -31,35 +31,8 @@ class DashboardTabpanels {
     }
     
     #createGeneral(container) {
-        let tableWell = document.createElement("div")
-        tableWell.className = "as-table-well"
-        
-        let table = document.createElement("table")
-        table.className = "table table-hover"
-        
-        let thead = document.createElement("thead")
-        let row = document.createElement("tr")
-        let th = document.createElement("th")
-        let headings = []
-        headings[0] = th.cloneNode()
-        headings[0].innerText = "Area"
-        headings[1] = th.cloneNode()
-        headings[1].innerText = "Status"
-        headings[2] = th.cloneNode()
-        headings[2].innerText = "Action"
-        row.append(headings[0], headings[1], headings[2])
-        thead.append(row)
-        
-        let tbody = document.createElement("tbody")
-        
-        // TODO: Pull these into here
-        generalAddScheduleRow(tbody)
-        generalAddPersonelManagementRow(tbody)
-        
-        table.append(thead, tbody)
-        tableWell.append(table)
-        
-        container.append(tableWell)
+        let general = new GeneralDashboard()
+        container.append(general.container)
     }
     
     async #createRouteManagement(container) {
@@ -273,6 +246,231 @@ async function getScheduleData(scheduleKey) {
     return result
 }
 
+class GeneralDashboard {
+    #container
+    #table
+    
+    constructor() {
+        this.#container = this.#createContainer()
+        this.#table = this.#createTable()
+        
+        this.#container.append(this.#table)
+    }
+    
+    #createContainer() {
+        let container = document.createElement("div")
+        container.className = "as-table-well"
+        
+        return container
+    }
+    
+    #createTable() {
+        let table = document.createElement("table")
+        table.className = "table table-hover"
+        
+        let thead = document.createElement("thead")
+        let cells = this.#createCells("th")
+        let row = this.#createRow(cells)
+        
+        cells[0].innerText = "Area"
+        cells[1].innerText = "Status"
+        cells[2].innerText = "Action"
+
+        thead.append(row)
+        let tbody = this.#createTableBody()
+
+        table.append(thead, tbody)
+        
+        return table
+    }
+    
+    #createTableBody() {
+        let tbody = document.createElement("tbody")
+        
+        this.#addScheduleRow(tbody)
+        this.#addPersonnelManagementRow(tbody)
+        
+        return tbody
+    }
+    
+    /**
+     * Adds a row for the schedule data status to the provided element
+     * @param {HTMLElement} tbody
+     */
+    async #addScheduleRow(tbody) {
+        let cells = this.#createCells("td")
+        let row = this.#createRow(cells)
+        
+        cells[0].innerText = "Schedule"
+        
+        // Create schedule button
+        let button = this.#createScheduleButton()
+        cells[2].append(button)
+        
+        // Compose schedule key
+        let server = AES.getServerName()
+        let airline = AES.getAirlineCode().code
+        let scheduleKey = `${server}${airline}schedule`
+        
+        // Get schedule data
+        let data = await chrome.storage.local.get([scheduleKey])
+        let scheduleData = data[scheduleKey]
+        
+        // Return instructions when no scheduleData is found
+        if (!scheduleData) {
+            cells[1].className = "bad"
+            cells[1].innerText = "No Schedule data found. Extract schedule or some AES parts will not work."
+            
+            tbody.append(row)
+            
+            return
+        }
+        
+        // TODO: pull getDate() into here/helpers.js
+        let lastUpdate = getDate("schedule", scheduleData.date)
+        let diff = AES.getDateDiff([todayDate.date, lastUpdate])
+        
+        cells[1].innerText = `Last schedule extract ${AES.formatDateString(lastUpdate)} (${diff} days ago). Extract new schedule if there are new routes.`
+        
+        if (diff >= 0 && diff < 7) {
+            cells[1].className = "good"
+        } else {
+            cells[1].className = "warning"
+        }
+        
+        tbody.append(row)
+    }
+    
+    /**
+     * Adds a row for the personnel update status to the provided element
+     * @param {HTMLElement} tbody
+     */
+    async #addPersonnelManagementRow(tbody) {
+        let cells = this.#createCells("td")
+        let row = this.#createRow(cells)
+        
+        cells[0].innerText = "Personnel Management"
+        
+        // Create schedule button
+        let button = this.#createPersonnelButton()
+        cells[2].append(button)
+        
+        // Compose schedule key
+        let server = AES.getServerName()
+        let airline = AES.getAirlineCode().name
+        let personnelKey = `${server}${airline}personelManagement`
+        
+        // Get personnel data
+        let data = await chrome.storage.local.get([personnelKey])
+        let personnelData = data[personnelKey]
+        
+        if (!personnelData) {
+            cells[1].innerText = "No personnel salary update date found."
+            cells[1].className = "bad"
+            
+            tbody.append(row)
+            
+            return
+        }
+        
+        let lastUpdate = personnelData.date
+        let diff = AES.getDateDiff([todayDate.date, lastUpdate])
+        cells[1].innerText = `Last personnel salary update: ${AES.formatDateString(lastUpdate)} (${diff} days ago).`
+        
+        if (diff >= 0 && diff < 7) {
+            cells[1].className = "good"
+        } else {
+            cells[1].className = "warning"
+        }
+        
+        tbody.append(row)
+    }
+    
+    /**
+     * Create a row with cells
+     * @param {array} of HTMLElements <th> | <td>
+     * @returns {HTMLElement} <tr> with nested cells
+     */
+    #createRow(cells) {
+        let row = document.createElement("tr")
+        
+        // Append all cells
+        for (const cell of cells) {
+            row.append(cell)
+        }
+        
+        return row
+    }
+    
+    /**
+     * Returns an array with three cells
+     * @param {string} type: "th" | "td"
+     * @returns {array} with three cells
+     */
+    #createCells(type) {
+        let cell = document.createElement(type)
+        let cells = [
+            cell.cloneNode(),
+            cell.cloneNode(),
+            cell.cloneNode()
+        ]
+        
+        return cells
+    }
+    
+    /**
+     * Create a <button> element
+     * @param {string} label
+     * @param {string} type
+     * @param {string} className
+     * @returns {HTMLElement} <button>
+     */
+    #createButton(label, type = "button", className = "btn btn-default") {
+        let button = document.createElement("button")
+        button.innerText = label
+        button.setAttribute("type", type)
+        button.className = className
+        
+        return button
+    }
+    
+    // Buttons
+    #createScheduleButton() {
+        let button = this.#createButton("extract schedule data")
+        
+        button.addEventListener("click", this.#scheduleButtonClickHandler)
+        
+        return button
+    }
+    
+    #scheduleButtonClickHandler(event) {
+        settings.schedule.autoExtract = 1
+        
+        let link = document.querySelector(".facts tfoot a[href$=\"tab\=3\"]")
+        
+        chrome.storage.local.set({settings: settings}, () => {
+            link.click()
+        })
+    }
+    
+    #createPersonnelButton() {
+        let button = this.#createButton("open personel management")
+        
+        button.addEventListener("click", this.#personnelButtonClickHandler)
+        
+        return button
+    }
+    
+    #personnelButtonClickHandler(event) {
+        let link = document.querySelector("a[href$=\"action/enterprise/staffOverview\"]")
+        
+        link.click()
+    }
+    
+    get container() {
+        return this.#container
+    }
+}
 // TODO: Split this out into a routeManagement/filters module
 function createTableHeader() {
     let thead = document.createElement("thead")

b0256191dec887562e965b087b3013e19ff0f3b7
Zoë Bijl <github@hello.moiety.me>
2024-06-06 16:10:02 +0200

    refactor: displayInventory
    displayInventory now uses await to get local storage

--- extension/content_inventory.js
+++ extension/content_inventory.js
@@ -14,77 +14,75 @@ window.addEventListener("load", async (event) => {
     }
 })
 
-function displayInventory() {
-    chrome.storage.local.get(['settings'], function(result) {
-        settings = result.settings;
-        todayDate = parseInt(getCurrentDateTime().date, 10);
-        //Get flights
-        let flights = getFlights();
-        let prices = getPriceDetails();
-        let storageKey = getPricingInventoryKey();
+async function displayInventory() {
+    const settingsData = await chrome.storage.local.get(['settings'])
+    settings = settingsData.settings
+
+    todayDate = parseInt(getCurrentDateTime().date, 10);
+    //Get flights
+    let flights = getFlights();
+    let prices = getPriceDetails();
+    let storageKey = getPricingInventoryKey();
     
-        //Check if any snapshots saved
-        let defaultPricingData = {
-            server: storageKey.server,
-            airline: storageKey.airline,
-            type: storageKey.type,
-            origin: storageKey.origin,
-            destination: storageKey.destination,
-            key: storageKey.key,
-            date: {}
-        }
-        chrome.storage.local.get({
-            [storageKey.key]: defaultPricingData }, function(result) {
-            pricingData = result[storageKey.key];
-            //Do Analysis
-            analysis = getAnalysis(flights, prices, pricingData.date);
-            //Display analysis
-            displayAnalysis(analysis, prices);
-            //Display history
-            displayHistory(analysis);
-    
-    
-            //Automation
-            //Check if valid analysis exists
-    
-            if (analysis.hasValue('valid')) {
-                //CHeck if updated todayDate
-                if (pricingData.date[todayDate]) {
-                    //Today update exists
-                    //Check if pricing updated today
-                    if (pricingData.date[todayDate].pricingUpdated) {
-                        //Pricing updated today
-                        //Do nothing
-                    } else {
-                        //Pricing not updated today
-                        //Check if new price avaialble
-                        if (analysis.hasValue('newPrice')) {
-                            //Update price
-                            if (settings.invPricing.autoPriceUpdate) {
-                                $('#aes-btn-invPricing-apply-new-prices').click();
-                            }
-                        }
-                    }
-                } else {
-                    //Today update does not exists
-                    //Check if new price avaialble
-                    if (analysis.hasValue('newPrice')) {
-                        //Update price
-                        if (settings.invPricing.autoPriceUpdate) {
-                            $('#aes-btn-invPricing-apply-new-prices').click();
-                        } else if (settings.invPricing.autoAnalysisSave) {
-                            $('#aes-btn-invPricing-save-snapshot').click();
-                        }
-                    } else {
-                        //Update data
-                        if (settings.invPricing.autoAnalysisSave) {
-                            $('#aes-btn-invPricing-save-snapshot').click();
-                        }
+    //Check if any snapshots saved
+    let defaultPricingData = {
+        server: storageKey.server,
+        airline: storageKey.airline,
+        type: storageKey.type,
+        origin: storageKey.origin,
+        destination: storageKey.destination,
+        key: storageKey.key,
+        date: {}
+    }
+    const storageData = await chrome.storage.local.get({[storageKey.key]: defaultPricingData})
+    pricingData = storageData[storageKey.key]
+        
+    //Do Analysis
+    analysis = getAnalysis(flights, prices, pricingData.date);
+    //Display analysis
+    displayAnalysis(analysis, prices);
+    //Display history
+    displayHistory(analysis);
+
+
+    //Automation
+    //Check if valid analysis exists
+    if (analysis.hasValue('valid')) {
+        //CHeck if updated todayDate
+        if (pricingData.date[todayDate]) {
+            //Today update exists
+            //Check if pricing updated today
+            if (pricingData.date[todayDate].pricingUpdated) {
+                //Pricing updated today
+                //Do nothing
+            } else {
+                //Pricing not updated today
+                //Check if new price avaialble
+                if (analysis.hasValue('newPrice')) {
+                    //Update price
+                    if (settings.invPricing.autoPriceUpdate) {
+                        $('#aes-btn-invPricing-apply-new-prices').click();
                     }
                 }
             }
-        });
-    });
+        } else {
+            //Today update does not exists
+            //Check if new price avaialble
+            if (analysis.hasValue('newPrice')) {
+                //Update price
+                if (settings.invPricing.autoPriceUpdate) {
+                    $('#aes-btn-invPricing-apply-new-prices').click();
+                } else if (settings.invPricing.autoAnalysisSave) {
+                    $('#aes-btn-invPricing-save-snapshot').click();
+                }
+            } else {
+                //Update data
+                if (settings.invPricing.autoAnalysisSave) {
+                    $('#aes-btn-invPricing-save-snapshot').click();
+                }
+            }
+        }
+    }
 }
 
 /**

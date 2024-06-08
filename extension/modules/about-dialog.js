669d8445f3bd9d40c9ac7f3ef9fcd076ba331cc1
Zoë Bijl <github@hello.moiety.me>
2024-06-08 19:44:29 +0200

    feat(#33): add about window

---
+++ extension/modules/about-dialog.js
@@ -0,0 +1,118 @@
+class AboutDialog {
+    #target
+    #container
+    #modalDialog
+    #modalContent
+    #header
+    #heading
+    #closeButton
+    #body
+
+    constructor() {
+        this.#body = this.#createBody()
+        this.#heading = this.#createHeading()
+        this.#closeButton = this.#createCloseButton()
+        this.#header = this.#createHeader()
+        this.#header.append(this.#closeButton, this.#heading)
+        this.#modalContent = this.#createModalContent()
+        this.#modalContent.append(this.#header, this.#body)
+        this.#modalDialog = this.#createModalDialog()
+        this.#modalDialog.append(this.#modalContent)
+        this.#container = this.#createContainer()
+        this.#container.append(this.#modalDialog)
+        this.#target = this.#setTarget()
+        this.#target.append(this.#container)
+        
+        const observer = this.#mutationObserver()
+        observer.observe(this.#container, {attributes: true})
+    }
+    
+    #createContainer() {
+        const container = document.createElement("div")
+        container.class = "modal"
+        container.id = "aes-about-dialog"
+        container.setAttribute("aria-hidden", "true")
+        container.style = "display: none"
+
+        return container
+    }
+    
+    #createModalDialog() {
+        const modalDialog = document.createElement("div")
+        modalDialog.className = "modal-dialog modal-md"
+        
+        return modalDialog
+    }
+    
+    #createModalContent() {
+        const modalContent = document.createElement("div")
+        modalContent.className = "modal-content"
+        
+        return modalContent
+    }
+    
+    #createHeader() {
+        const header = document.createElement("div")
+        header.className = "modal-header"
+
+        return header
+    }
+    
+    #createHeading() {
+        const heading = document.createElement("h1")
+        heading.className = "modal-title"
+        heading.innerText = "About the AirlineSim Enhancement Suite"
+
+        return heading
+    }
+    
+    #createCloseButton() {
+        const icon = document.createElement("span")
+        icon.setAttribute("aria-hidden", "true")
+        icon.innerText = "×"
+        const label = document.createElement("span")
+        label.innerText = "Close"
+        label.className = "sr-only"
+        const button = document.createElement("button")
+        button.setAttribute("type", "button")
+        button.dataset.dismiss = "modal"
+        button.className = "close"
+        button.append(icon, label)
+
+        return button
+    }
+    
+    #createBody() {
+        const body = document.createElement("div")
+        body.className = "modal-body"
+        const manifest = chrome.runtime.getManifest()
+        const description = manifest.description
+        const version = manifest.version
+        
+        body.innerHTML = `
+            <p>${description}</p>
+            <p>You’re running AES v${version}</p>
+        `
+        
+        return body
+    }
+    
+    #setTarget() {
+        const target = document.querySelector("body")
+        return target
+    }
+    
+    #mutationObserver() {
+        const observer = new MutationObserver(this.#correctBootstrapBehaviour.bind(this))
+        return observer
+    }
+    
+    #correctBootstrapBehaviour() {
+        if (this.#container.classList.contains("in") && !this.#container.classList.contains("modal")) {
+            this.#container.classList.add("modal")
+            this.#container.style = "display: block"
+        }
+    }
+}
+
+new AboutDialog()

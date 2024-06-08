740e9e37cd89d7eefa953050bd3afdfa81664d42
Zoë Bijl <github@hello.moiety.me>
2024-06-08 03:15:14 +0200

    feat: add AES menu
    Adds a menu item to the main game menu. Provides easy access to the forum topic and reporting bugs.

---
+++ extension/modules/aes-menu.js
@@ -0,0 +1,109 @@
+class AESMenu {
+    #target
+    #container
+    #button
+    #menu
+
+    constructor() {
+        this.#target = document.querySelector("#as-navbar-main-collapse .navbar-nav > li:nth-child(5)")
+        this.#container = this.#createContainer()
+        this.#button = this.#createButton()
+        this.#menu = this.#createMenu()
+        
+        this.#container.append(this.#button, this.#menu)
+        this.#target.after(this.#container)
+    }
+    
+    /**
+     * Creates the menu container
+     * @returns {HTMLElement} container
+     */
+    #createContainer() {
+        const container = document.createElement("li")
+        container.className = "dropdown"
+        return container
+    }
+    
+    /**
+     * Creates the menu toggle button
+     * @returns {HTMLElement} button
+     */
+    #createButton() {
+        const caret = document.createElement("span")
+        caret.className = "caret"
+        const button = document.createElement("a")
+        button.setAttribute("role", "button")
+        button.setAttribute("tabindex", "0")
+        button.dataset.toggle = "dropdown"
+        button.className = "dropdown-toggle"
+        button.innerText = "AES"
+        button.style = "cursor: pointer"
+        button.append(caret)
+
+        return button
+    }
+    
+    /**
+     * Creates the menu
+     * @returns {HTMLElement} menu
+     */
+    #createMenu() {
+        const menu = document.createElement("ul")
+        menu.className = "dropdown-menu"
+        const menuItems = []
+        const content = [{
+            label: "Forum Topic",
+            href: "https://forums.airlinesim.aero/t/introducing-airlinesim-enhancement-suite-beta/",
+            newWindow: true
+        },{
+            label: "Report a Bug",
+            href: `https://github.com/ZoeBijl/airlinesim-enhancement-suite/issues/new?body=AES: v${chrome.runtime.getManifest().version}%0AChrome: v${window.navigator.userAgent.match(/Chrom(?:e|ium)\/([0-9]+)/)[1]}%0A%0A`,
+            newWindow: true
+        }]
+        for (const item of content) {
+            let menuItem = this.#createMenuItem(item)
+            menuItems.push(menuItem)
+        }
+        for (const item of menuItems) {
+            menu.append(item)
+        }
+        
+        return menu
+    }
+    
+    /**
+     * Creates a menu item
+     * @param {object} content - object containing information required to build the item
+     * @returns {HTMLElement} menuItem
+     */
+    #createMenuItem(content) {
+        const menuItem = document.createElement("li")
+        let menuItemContent
+        if (content.href) {
+            const link = document.createElement("a")
+            link.setAttribute("href", content.href)
+            let icon
+            if (content.icon || content.newWindow) {
+                icon = document.createElement("span")
+            }
+            if (content.icon) {
+                icon.className = content.icon.className
+            }
+            if (content.newWindow) {
+                link.setAttribute("target", "_blank")
+                link.setAttribute("rel", "noreferrer noopener")
+                icon.className = "fa fa-external-link"
+            }
+            if (icon) {
+                link.append(icon)
+                link.append(" ")
+            }
+            link.append(content.label)
+            menuItemContent = link
+        }
+        menuItem.append(menuItemContent)
+        return menuItem
+    }
+}
+
+new AESMenu()

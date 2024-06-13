f20bf5ff8d5dec9ee06afd2d09de20bcbab17ab9
Zoë Bijl <github@hello.moiety.me>
2024-06-13 02:38:38 +0200

    add ExtractionButton class

---
+++ extension/modules/aircraft-flights/extraction-button.js
@@ -0,0 +1,25 @@
+class ExtractionButton {
+    element
+    label
+    callback
+    className
+    type
+
+    constructor(label, callback, className = "btn btn-default", type = "button") {
+        this.label = label
+        this.callback = callback
+        this.className = className
+        this.type = type
+
+        this.element = this.#createElement()
+    }
+    
+    #createElement() {
+        const button = document.createElement("button")
+        button.type = this.type
+        button.innerText = this.label
+        button.className = this.className
+        
+        return button
+    }
+}

945adf0f9de18002782116cae62024a5f1fd8886
Zoë Bijl <github@hello.moiety.me>
2024-06-13 02:40:05 +0200

    add Aircraft class

---
+++ extension/modules/aircraft-flights/aircraft-data.js
@@ -0,0 +1,29 @@
+class Aircraft {
+    id
+    type
+    registration
+    flights
+    datetime
+    server
+    currentFlights = {
+        finished: null,
+        total: null
+    }
+
+    constructor() {
+        this.datetime = AES.getServerDate()
+    }
+}
+
+let aircraftExample = {
+    aircraftId: 27623,
+    date: "20240612",
+    equipment: "Embraer 190 E2",
+    finishedFlights: 0,
+    flights: [],
+    registration: "VH-ADS",
+    server: "tristar",
+    time: "21:48 UTC",
+    totalFlights: 9,
+    type: "aircraftFlights"
+}

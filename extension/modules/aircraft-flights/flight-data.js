4c65ff5a350d1d1d78816b8ec1161b6b1d990d7e
Zoë Bijl <github@hello.moiety.me>
2024-06-13 02:37:05 +0200

    add flight-data class

---
+++ extension/modules/aircraft-flights/flight-data.js
@@ -0,0 +1,21 @@
+class FlightData {
+    id
+    flightNumber
+    status
+    row
+    data
+    
+    /**
+     * Creates a FlightData object
+     * @param {integer} id - 5322 | 912
+     * @param {string} flightNumber - "MOI 1000" | "OZ 22"
+     * @param {string} status - "booked" | "inflight" | "finished" | "cancelled"
+     * @param {HTMLObject} row - element the data is extracted from
+     */
+    constructor(id, flightNumber, status, row) {
+        this.id = id
+        this.flightNumber = flightNumber
+        this.status = status
+        this.row = row
+    }
+}

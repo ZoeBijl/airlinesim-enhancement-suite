ad86276802b3870514e2c12f2bdc79f64d630eba
Zoë Bijl <github@hello.moiety.me>
2024-06-14 12:12:30 +0200

    expand FlightData jsdoc

--- extension/modules/data-models/flight-data.js
+++ extension/modules/data-models/flight-data.js
@@ -8,18 +8,21 @@ class FlightData {
     financials
     isCancellable
     data
-    
+
     /**
      * Creates a FlightData object
      * @param {integer} id - 5322 | 912
      * @param {string} flightNumber - "MOI 1000" | "OZ 22"
      * @param {string} status - "booked" | "inflight" | "finished" | "cancelled"
+     * @param {object} financials - object containing financial data
+     * @param {boolean} isCancellable - indicates the presence of a checkbox / if the flight is either finished or inflight
      * @param {HTMLObject} row - element the data is extracted from
      */
-    constructor(id, flightNumber, status, isCancellable, row) {
+    constructor(id, flightNumber, status, financials, isCancellable, row) {
         this.id = id
         this.flightNumber = flightNumber
         this.status = status
+        this.financials = financials
         this.isCancellable = isCancellable
         this.row = row
     }
